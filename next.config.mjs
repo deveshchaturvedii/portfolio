27621c6a2fca5264525c158c50fb88ce7204e8a4
Shruti Chaturvedi <shrutichaturvedi@Shrutis-MacBook-Air.local>
2026-04-08 10:32:48 -0300

    Pushing the config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
+};
 
-export default nextConfig
+export default nextConfig;
